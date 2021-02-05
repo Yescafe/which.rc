5c594a6470f181a7cc0e90038095b398d7ef6717
Ivan Chien <qyc027@gmail.com>
2021-02-05 16:55:26 +0800

    Auto commit at 2021/02/05 16时55分26秒

--- auto-ln.sh
+++ auto-ln.sh
@@ -7,9 +7,19 @@ else
 fi
 basepath=$(cd $BASE_DIR; pwd)
 echo auto-ln.sh at $basepath
-for f in `ls -a | grep rc`; do
+for f in `ls -a | grep rc | grep -v zsh`; do
   echo "Remove ~/$f and rebuild"
   rm -f ~/$f > /dev/null
   ln -s $basepath/$f ~/$f > /dev/null
 done
 
+while true; do
+  read -p "Use oh-my-zsh? (y/N): " omz
+  echo "Remove ~/.zshrc and rebuild"
+  rm -f ~/.zshrc > /dev/null
+  case $omz in
+    [yY]*) ln -s $basepath/.omzshrc ~/.zshrc > /dev/null; break;;
+    [nN]*) ln -s $basepath/.zshrc   ~/.zshrc > /dev/null; break;;
+    * ) echo "Please answer y or n.";;
+  esac
+done
