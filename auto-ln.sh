f4ebbb5314fd208ef180b693e52cfec612c72b74
Ivan Chien <qyc027@gmail.com>
2021-02-05 17:46:36 +0800

    Auto commit at 2021/02/05 17时46分36秒

--- auto-ln.sh
+++ auto-ln.sh
@@ -5,21 +5,27 @@ then
 else
     BASE_DIR=`dirname $0`
 fi
+timestamp=$(date +%s)
 basepath=$(cd $BASE_DIR; pwd)
+
+mkdir -p $basepath/bak-$timestamp > /dev/null
+
 echo auto-ln.sh at $basepath
 for f in `ls -a | grep rc | grep -v zsh`; do
   echo "Remove ~/$f and rebuild"
-  rm -f ~/$f > /dev/null
+  mv ~/$f $basepath/bak-$timestamp > /dev/null
   ln -s $basepath/$f ~/$f > /dev/null
 done
 
 while true; do
   read -p "Use oh-my-zsh? (y/N): " omz
   echo "Remove ~/.zshrc and rebuild"
-  rm -f ~/.zshrc > /dev/null
+  mv ~/.zshrc $basepath/bak-$timestamp > /dev/null
   case $omz in
     [yY]*) ln -s $basepath/.omzshrc ~/.zshrc > /dev/null; break;;
     [nN]*) ln -s $basepath/.zshrc   ~/.zshrc > /dev/null; break;;
     * ) echo "Please answer y or n.";;
   esac
 done
+
+echo "Backups at $basepath/bak-$timestamp"
