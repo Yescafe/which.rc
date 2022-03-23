c2d5f3c2a13f6a98e7e021f8c120e80a0e79248c
Ivan Chien <qyc027@gmail.com>
2022-03-23 12:02:15 +0800

    Add .vim

--- auto-ln.sh
+++ auto-ln.sh
@@ -13,8 +13,12 @@ mkdir -p $basepath/bak-$timestamp > /dev/null
 echo auto-ln.sh at $basepath
 for f in `ls -a | grep rc`; do
   echo "Remove ~/$f and rebuild"
-  mv ~/$f $basepath/bak-$timestamp > /dev/null
+  mv ~/$f $basepath/bak-$timestamp > /dev/null 2> /dev/null
   ln -s $basepath/$f ~/$f > /dev/null
 done
+
+echo "Remove ~/.vim and rebuild"
+mv ~/.vim $basepath/bak-$timestamp > /dev/null 2> /dev/null
+ln -s $basepath/.vim ~/.vim > /dev/null
 
 echo "Backups at $basepath/bak-$timestamp"
