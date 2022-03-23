c3d16c4346acff581fb7ff13474f28eec3b7300c
Ivan Chien <qyc027@gmail.com>
2022-03-23 11:53:06 +0800

    Remove non-omz zsh config

--- auto-ln.sh
+++ auto-ln.sh
@@ -11,21 +11,10 @@ basepath=$(cd $BASE_DIR; pwd)
 mkdir -p $basepath/bak-$timestamp > /dev/null
 
 echo auto-ln.sh at $basepath
-for f in `ls -a | grep rc | grep -v zsh`; do
+for f in `ls -a | grep rc`; do
   echo "Remove ~/$f and rebuild"
   mv ~/$f $basepath/bak-$timestamp > /dev/null
   ln -s $basepath/$f ~/$f > /dev/null
-done
-
-while true; do
-  read -p "Use oh-my-zsh? (y/N): " omz
-  echo "Remove ~/.zshrc and rebuild"
-  mv ~/.zshrc $basepath/bak-$timestamp > /dev/null
-  case $omz in
-    [yY]*) ln -s $basepath/.omzshrc ~/.zshrc > /dev/null; break;;
-    [nN]*) ln -s $basepath/.zshrc   ~/.zshrc > /dev/null; break;;
-    * ) echo "Please answer y or n.";;
-  esac
 done
 
 echo "Backups at $basepath/bak-$timestamp"
